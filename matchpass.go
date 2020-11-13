fcc4ecded76e1fb0338b33a85f7689c7501121bb
Akash Kumar <67784046+akashkumar8@users.noreply.github.com>
2020-11-13 14:01:03 +0530

    Password matching

--- matchpass.go
+++ matchpass.go
@@ -1,28 +1,70 @@
 package main
 
 import (
-    "fmt"
-
-    "golang.org/x/crypto/bcrypt"
+	"fmt"
+	"strings"
+	"unicode"
 )
 
-func HashPassword(password string) (string, error) {
-    bytes, err := bcrypt.GenerateFromPassword([]byte(password), 14)
-    return string(bytes), err
-}
+func verifyPassword(password string) error {
+	var uppercasePresent bool
+	var lowercasePresent bool
+	var numberPresent bool
+	var specialCharPresent bool
+	const minPassLength = 8
+	const maxPassLength = 64
+	var passLen int
+	var errorString string
 
-func CheckPasswordHash(password, hash string) bool {
-    err := bcrypt.CompareHashAndPassword([]byte(hash), []byte(password))
-    return err == nil
+	for _, ch := range password {
+		switch {
+		case unicode.IsNumber(ch):
+			numberPresent = true
+			passLen++
+		case unicode.IsUpper(ch):
+			uppercasePresent = true
+			passLen++
+		case unicode.IsLower(ch):
+			lowercasePresent = true
+			passLen++
+		case unicode.IsPunct(ch) || unicode.IsSymbol(ch):
+			specialCharPresent = true
+			passLen++
+		case ch == ' ':
+			passLen++
+		}
+	}
+	appendError := func(err string) {
+		if len(strings.TrimSpace(errorString)) != 0 {
+			errorString += ", " + err
+		} else {
+			errorString = err
+		}
+	}
+	if !lowercasePresent {
+		appendError("lowercase letter missing")
+	}
+	if !uppercasePresent {
+		appendError("uppercase letter missing")
+	}
+	if !numberPresent {
+		appendError("atleast one numeric character required")
+	}
+	if !specialCharPresent {
+		appendError("special character missing")
+	}
+	if !(minPassLength <= passLen && passLen <= maxPassLength) {
+		appendError(fmt.Sprintf("password length must be between %d to %d characters long", minPassLength, maxPassLength))
+	}
+
+	if len(errorString) != 0 {
+		return fmt.Errorf(errorString)
+	}
+	return nil
 }
 
 func main() {
-    password := "secret"
-    hash, _ := HashPassword(password) 
-
-    fmt.Println("Password:", password)
-    fmt.Println("Hash:    ", hash)
-
-    match := CheckPasswordHash(password, hash)
-    fmt.Println("Match:   ", match)
+	password := "Samsung"
+	err := verifyPassword(password)
+	fmt.Println(password, " ", err)
 }
